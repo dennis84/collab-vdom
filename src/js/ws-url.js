78749dffed5851c2c798306b8125f7fa55f54ba6
Dennis Dietrich <d.dietrich84@gmail.com>
2014-10-18 18:18:17 +0200

    change ws-url

--- src/js/ws-url.js
+++ src/js/ws-url.js
@@ -1,5 +1,5 @@
 if('localhost' === window.location.hostname) {
   module.exports = 'ws://localhost:9000'
 } else {
-  module.exports = 'wss://polar-woodland-4270.herokuapp.com'
+  module.exports = 'wss://radiant-dusk-8167.herokuapp.com'
 }
