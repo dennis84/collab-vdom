833cedaf3d39a7adf631066762e42793e5a4bfe8
Dennis Dietrich <d.dietrich84@gmail.com>
2014-12-19 11:41:02 +0100

    small changes

--- src/js/scroll-to.js
+++ src/js/scroll-to.js
@@ -2,7 +2,7 @@ var scroll = require('scroll')
 
 function scrollTo(node, pos) {
   var offset = window.innerHeight * 0.3
-  scroll.top(node, (pos - 1) * 23 - offset)
+  scroll.top(node, (pos - 1) * 24 - offset)
 }
 
 module.exports = scrollTo
