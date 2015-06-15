de12b6036e15ee8443e0faf46cf82c6f747e0c55
Dennis Dietrich <d.dietrich84@gmail.com>
2015-06-15 20:13:41 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 var gulp = require('gulp')
   , gutil = require('gulp-util')
   , browserify = require('browserify')
+  , watchify = require('watchify')
   , source = require('vinyl-source-stream')
   , streamify = require('gulp-streamify')
   , less = require('gulp-less')
   , uglify = require('gulp-uglify')
 
+var onError = function(error) {
+  gutil.log(gutil.colors.red(error.message))
+}
+
 gulp.task('js', function() {
-  return browserify('./src/js/index.js')
-    .transform('brfs')
-    .bundle()
-    .on('error', gutil.log)
-    .pipe(source('index.js'))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest('./assets/js'))
+  var bundleStream = watchify(browserify('./src/js/index.js'))
+    .on('update', rebundle)
+    .on('log', gutil.log)
+
+  function rebundle() {
+    return bundleStream.transform('brfs').bundle()
+      .on('error', onError)
+      .pipe(source('index.js'))
+      .pipe(streamify(uglify()))
+      .pipe(gulp.dest('./assets/js'))
+  }
+
+  return rebundle()
 })
 
 gulp.task('less', function() {
@@ -22,8 +33,4 @@ gulp.task('less', function() {
     .pipe(gulp.dest('./assets/css'))
 })
 
-gulp.task('watch', function() {
-  gulp.watch(['index.html', 'src/**'], ['js', 'less'])
-})
-
-gulp.task('default', ['js', 'less', 'watch'])
+gulp.task('default', ['js', 'less'])
